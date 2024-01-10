1ee0907308f1b5479a13f04c69ac89d489ee4569
Ahmet Arif Akıncı <ahmetarifaknci@gmail.com>
2024-01-10 23:47:28 +0300

    init

---
+++ src/main/java/com/example/energy_consultation/EnergyConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.example.energy_consultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyConsultationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyConsultationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/energy_consultation/EnergyConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.energy_consultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyConsultationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
